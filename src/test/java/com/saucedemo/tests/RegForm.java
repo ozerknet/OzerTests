e08a77d50778d38cab840d7e76e38dadd680f4a7
ramazanyildirim1 <97792743+ramazanyildirim1@users.noreply.github.com>
2022-06-09 23:52:43 +0100

    RegForm  second commit

---
+++ src/test/java/com/saucedemo/tests/RegForm.java
@@ -0,0 +1,15 @@
+package com.saucedemo.tests;
+
+import org.testng.annotations.Test;
+
+public class RegForm {
+    @Test
+    public void regformTest(){
+        RegForm regForm= new RegForm();
+
+
+    }
+
+
+
+}
