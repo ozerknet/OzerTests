6197a3129982c0542475c29b31c6f0eb78b6cda4
Mehmet Akif Demircan <mademircan1014@gmail.com>
2022-06-10 20:47:47 +0100

    Contact us form reset and submit

--- src/test/java/com/webdriveruniversity/AkifTest.java
+++ src/test/java/com/webdriveruniversity/AkifTest.java
@@ -3,45 +3,45 @@ package com.webdriveruniversity;
 import com.github.javafaker.Faker;
 import com.utilities.Driver;
 import com.utilities.WebsitesReader;
+import com.webdriveruniversity.pages.contactUs;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.openqa.selenium.support.PageFactory;
 
 public class AkifTest {
 
+    contactUs contactUs = new contactUs();
 
     @Test (priority = 1, description = "Testing contact us form reset functionality")
     public void resetTesting() {
 
         Driver.getDriver().get(WebsitesReader.getProperty("contactus"));
 
-        //Locating all contact us form elements
-        WebElement firstName = Driver.getDriver().findElement(By.xpath("//input[@name='first_name']"));
-        WebElement lastName = Driver.getDriver().findElement(By.xpath("//input[@name='last_name']"));
-        WebElement email = Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
-        WebElement comments = Driver.getDriver().findElement(By.xpath("//textarea[@name='message']"));
-
         Faker faker = new Faker();
 
         //Complete all the form by random inputs
         String fakerFirstName = faker.name().firstName();
-        firstName.sendKeys(fakerFirstName);
+        contactUs.firstName.sendKeys(fakerFirstName);
 
         String fakerLastName = faker.name().lastName();
-        lastName.sendKeys(fakerLastName);
+        contactUs.last_name.sendKeys(fakerLastName);
 
-        email.sendKeys(fakerFirstName+fakerLastName+"@gmail.com");
+        contactUs.email.sendKeys(fakerFirstName+fakerLastName+"@gmail.com");
 
         String fakerComments = faker.lorem().paragraph();
-        comments.sendKeys(fakerComments);
+        contactUs.message.sendKeys(fakerComments);
 
 
         WebElement resetButton = Driver.getDriver().findElement(By.xpath("//input[@type='reset']"));
         resetButton.click();
 
-       Assert.assertTrue((firstName.getText().isEmpty()&&(lastName.getText().isEmpty())&&(email.getText().isEmpty())&&(comments.getText().isEmpty())));
+       Assert.assertTrue((contactUs.firstName.getText().isEmpty()
+               &&(contactUs.last_name.getText().isEmpty())
+               &&(contactUs.email.getText().isEmpty())
+               &&(contactUs.message.getText().isEmpty())));
 
     }
 
@@ -50,25 +50,18 @@ public class AkifTest {
 
         Driver.getDriver().get(WebsitesReader.getProperty("contactus"));
 
-        //Locating all contact us form elements
-        WebElement firstName = Driver.getDriver().findElement(By.xpath("//input[@name='first_name']"));
-        WebElement lastName = Driver.getDriver().findElement(By.xpath("//input[@name='last_name']"));
-        WebElement email = Driver.getDriver().findElement(By.xpath("//input[@name='email']"));
-        WebElement comments = Driver.getDriver().findElement(By.xpath("//textarea[@name='message']"));
-
         Faker faker = new Faker();
 
-        //Complete all the form by random inputs
         String fakerFirstName = faker.name().firstName();
-        firstName.sendKeys(fakerFirstName);
+        contactUs.firstName.sendKeys(fakerFirstName);
 
         String fakerLastName = faker.name().lastName();
-        lastName.sendKeys(fakerLastName);
+        contactUs.last_name.sendKeys(fakerLastName);
 
-        email.sendKeys(fakerFirstName+fakerLastName+"@gmail.com");
+        contactUs.email.sendKeys(fakerFirstName+fakerLastName+"@gmail.com");
 
         String fakerComments = faker.lorem().paragraph();
-        comments.sendKeys(fakerComments);
+        contactUs.message.sendKeys(fakerComments);
 
 
         WebElement submitButton = Driver.getDriver().findElement(By.xpath("//input[@type='submit']"));
