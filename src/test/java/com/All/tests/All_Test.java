cface89fdb54931fc08cfa46db59c0b0a25b5928
Ozer <ozerknet@gmail.com>
2022-06-10 19:50:28 +0100

    alllink test finished

--- src/test/java/com/All/tests/All_Test.java
+++ src/test/java/com/All/tests/All_Test.java
@@ -2,6 +2,7 @@ package com.All.tests;
 
 import com.utilities.Driver;
 import com.utilities.WebsitesReader;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.BeforeMethod;
@@ -21,8 +22,11 @@ public class All_Test {
 
         PageFactory.initElements(Driver.getDriver(), this);
 
-
-        List<WebElement> allLink = new ArrayList<>();
+        List <WebElement> allLinks =  Driver.getDriver().findElements(By.xpath("//div[@class='entry-content']/ul[1]/li/strong/a"));
+        System.out.println("allLinks.size() = " + allLinks.size());
+        for (WebElement allLink : allLinks) {
+            System.out.println("allLink = " + allLink.getAttribute("href"));
+        }
 
 
 
